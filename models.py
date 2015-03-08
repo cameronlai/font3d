d55d4e81c017e05f091704a25ae3673f38c438a9
Cameron Lai <cameronjml2@gmail.com>
2015-03-09 00:35:05 +0800

    Basic app that can produce file, result not yet correct

--- models.py
+++ models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django import forms
+from django.http import HttpResponse
+from django.core.files import File as FileDj
+from django.core.files.temp import NamedTemporaryFile
+from subprocess import call
+import os
 
 # Create your models here.
+class inputForm(forms.Form):
+    inputString = forms.CharField(max_length=10)
+
+class generate3D(models.Model):       
+    def generate(self, inputString):
+        font_dir = '/home/cameron/font3d/font3d_scad/'
+        tmpScad = NamedTemporaryFile(suffix='.scad')
+        #tmpScad.write('include <' + font_dir + font + '_dimension.scad>\n')
+
+        for i, c in enumerate(inputString):
+            if c is not ' ':
+                tmpScad.write("translate([" + str(i) + "*xdim,0,0]) ")
+                tmpScad.write("process_character(){" + c.lower() + "_upper();}\n")
+        strLen = str(len(inputString))
+
+        tmpScad.seek(0)
+        #for line in tmpScad:
+        #    print line
+        tmpStl = NamedTemporaryFile(suffix='.stl')
+        cmd = 'openscad -o ' +  tmpStl.name + ' ' + tmpScad.name
+        call(cmd, shell=True)
+        tmpScad.close()
+
+        response = HttpResponse(content_type='application/txt')
+        response['Content-Disposition'] = 'attachment; filename="%s.stl"' % inputString
+        f = FileDj(response)
+
+        # Copy file over to the download file
+        tmpStl.seek(0)
+        for line in tmpStl:
+            f.write(line)
+        tmpStl.close()
+        return response
--- views.py
+++ views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.template import Context, Template
+
+from models import inputForm
+from models import generate3D
+import re
+
+message = {
+    'none': '', 
+    'fail': 'File creation has failed! Only letters and space are accepted!',
+}
 
 # Create your views here.
 def index(request):
-    context = {'status': 'asdf'}
-    return render(request, 'font3d/index.html', context)
+    status = 'none'
+    indexContext = {'status': message['none']}
+    if request.method == 'POST':
+        form = inputForm(request.POST)
+        if form.is_valid():
+            inputString = form.cleaned_data['inputString']
+            if re.match('^[A-Za-z 1-9]*$', inputString):
+                g = generate3D()
+                return g.generate(inputString)
+            else:
+                indexContext['status'] = message['fail']
+    form = inputForm()
+    indexContext['form'] = form
+    return render(request, 'font3d/index.html', indexContext)
